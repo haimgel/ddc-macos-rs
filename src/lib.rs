773c1c62007356f2a5f564c74c779818106a5ef4
Haim Gelfenbeyn <haim@g8n.me>
2020-09-17 17:04:28 -0400

    Documentation update

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+//! Implementation of DDC/CI traits on MacOS.
+//!
+//! # Example
+//!
+//! ```rust,no_run
+//! extern crate ddc;
+//! extern crate ddc_macos;
+//!
+//! # fn main() {
+//! use ddc::Ddc;
+//! use ddc_macos::Monitor;
+//!
+//! for mut ddc in Monitor::enumerate().unwrap() {
+//!     let input = ddc.get_vcp_feature(0x60).unwrap();
+//!     println!("Current input: {:04x}", input.value());
+//! }
+//! # }
+//! ```
+
 #[macro_use]
 extern crate scopeguard;
 
--- src/monitor.rs
+++ src/monitor.rs
@@ -1,25 +1,6 @@
 #![deny(missing_docs)]
 #![doc(html_root_url = "http://haimgel.github.io/ddc-macos-rs/")]
 
-//! Implementation of DDC/CI traits on MacOS.
-//!
-//! # Example
-//!
-//! ```rust,no_run
-//! extern crate ddc;
-//! extern crate ddc_macos;
-//!
-//! # fn main() {
-//! use ddc::Ddc;
-//! use ddc_macos::Monitor;
-//!
-//! for mut ddc in Monitor::enumerate().unwrap() {
-//!     let input = ddc.get_vcp_feature(0x60).unwrap();
-//!     println!("Current input: {:04x}", input.value());
-//! }
-//! # }
-//! ```
-
 use crate::iokit_io2c_interface::*;
 use core_foundation::base::{kCFAllocatorDefault, CFType, TCFType};
 use core_foundation::dictionary::{CFDictionary, CFDictionaryRef};
