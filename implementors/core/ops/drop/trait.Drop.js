8641bc15b9295ed455efb3c8447704f7ef927269
haimgel <haimgel@users.noreply.github.com>
2020-09-20 14:48:27 +0000

    deploy: 6b266a5e5540b2c20f58b532e5de46c08e5ee1b5

--- implementors/core/ops/drop/trait.Drop.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["ddc_macos"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"ddc_macos/struct.Monitor.html\" title=\"struct ddc_macos::Monitor\">Monitor</a>","synthetic":false,"types":["ddc_macos::monitor::Monitor"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
